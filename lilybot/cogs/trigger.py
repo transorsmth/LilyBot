464a03cfb2068df5537a23837e69cea9ee98767e
Ava <transorsmth@gmail.com>
2023-12-17 19:08:24 -0500

    Debug for keysmash

--- lilybot/cogs/trigger.py
+++ lilybot/cogs/trigger.py
@@ -30,9 +30,9 @@ class Trigger(Cog):
         if message.guild.id == 983814962822660176:
             print("Correct guild")
             print(detect_keysmash.is_mashing(message.content))
-            print(await message.guild.fetch_role(
+            print(message.guild.get_role(
                     983824647856472154) in message.author.roles)
-            if detect_keysmash.is_mashing(message.content) and await message.guild.fetch_role(
+            if detect_keysmash.is_mashing(message.content) and message.guild.get_role(
                     983824647856472154) in message.author.roles:
                 await message.channel.send("good girl")
 
