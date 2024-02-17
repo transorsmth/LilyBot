d63964d53ab74f539caf1921f24f52a876a9f039
Ava <111925667+transorsmth@users.noreply.github.com>
2024-02-17 16:27:13 -0500

    Update detect_keysmash.py

--- lilybot/components/detect_keysmash.py
+++ lilybot/components/detect_keysmash.py
@@ -81,7 +81,7 @@ def is_mashing(text: str, cutoff=1.75):
     return score_not_mashing(text) < cutoff
 
 
-ignorefilter = ['mississippi', 'triggering', 'interesting', "relatable"]
+ignorefilter = ['mississippi', 'triggering', 'interesting', "relatable", "disengage"]
 
 
 def is_keysmash(text):
